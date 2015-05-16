ab18f8fdc1695267e032edc0a02d1a86e21fcdec
Sandip Bhagat <sandipbgt@gmail.com>
2015-05-16 09:10:07 +0545

    Added list of zones with their districts in PHP array format

---
+++ php/zones_with_districts.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * @author Sandip Bhagat
+ * @website www.sandipbhagat.com
+ * @twitter sandipbgt
+ * @facebook sandip02
+ * @github https://github.com/sandipbgt/nepal-data
+ */
+
+$data = array(
+	"bagmati" => array(
+				"bhaktapur",
+				"dhading",
+				"kathmandu",
+				"kavrepalanchok",
+				"lalitpur",
+				"nuwakot",
+				"rasuwa",
+				"sindhupalchok",
+			
+		),
+	"bheri" => array(
+				"banke",
+				"bardiya",
+				"dailekh",
+				"jajarkot",
+				"surkhet",
+			
+		),
+	"dhawalagiri" => array(
+				"baglung",
+				"mustang",
+				"myagdi",
+				"parbat",
+			
+		),
+	"gandaki" => array(
+				"gorkha",
+				"kaski",
+				"lamjung",
+				"manang",
+				"syangja",
+				"tanahu",
+			
+		),
+	"janakpur" => array(
+				"dhanusa",
+				"dholkha",
+				"mahottari",
+				"ramechhap",
+				"sarlahi",
+				"sindhuli",
+			
+		),
+	"karnali" => array(
+				"dolpa",
+				"humla",
+				"jumla",
+				"kalikot",
+				"mugu",
+			
+		),
+	"koshi" => array(
+				"bhojpur",
+				"dhankuta",
+				"morang",
+				"sankhuwasabha",
+				"sunsari",
+				"terhathum",
+			
+		),
+	"lumbini" => array(
+				"arghakhanchi",
+				"gulmi",
+				"kapilvastu",
+				"nawalparasi",
+				"palpa",
+				"rupandehi",
+			
+		),
+	"mahakali" => array(
+				"baitadi",
+				"dadeldhura",
+				"darchula",
+				"kanchanpur",
+			
+		),
+	"mechi" => array(
+				"ilam",
+				"jhapa",
+				"panchthar",
+				"taplejung",
+			
+		),
+	"narayani" => array(
+				"bara",
+				"chitwan",
+				"makwanpur",
+				"parsa",
+				"rautahat",
+			
+		),
+	"rapti" => array(
+				"dang deukhuri",
+				"pyuthan",
+				"rolpa",
+				"rukum",
+				"salyan",
+			
+		),
+	"sagarmatha" => array(
+				"khotang",
+				"okhaldhunga",
+				"saptari",
+				"siraha",
+				"solukhumbu",
+				"udayapur",
+			
+		),
+	"seti" => array(
+				"achham",
+				"bajhang",
+				"bajura",
+				"doti",
+				"kailali",
+			
+		)
+);
+?>
