50899a4bcdbf0752861e4c29d6f955fcb6c4f107
Sandip Bhagat <sandipbgt@gmail.com>
2015-05-16 09:10:47 +0545

    Added list of zones with their districts and headquarters in PHP array format

---
+++ php/zones_with_districts_headquarters.php
@@ -0,0 +1,354 @@
+<?php
+/**
+ * @author Sandip Bhagat
+ * @website www.sandipbhagat.com
+ * @twitter sandipbgt
+ * @facebook sandip02
+ * @github https://github.com/sandipbgt/nepal-data
+ */
+
+$data = array(
+	"bagmati" => array(
+			array(
+				"district" => "bhaktapur",
+				"headquarter" => "bhaktapur"
+			),
+			array(
+				"district" => "dhading",
+				"headquarter" => "dhading besi"
+			),
+			array(
+				"district" => "kathmandu",
+				"headquarter" => "kathmandu"
+			),
+			array(
+				"district" => "kavrepalanchok",
+				"headquarter" => "dhulikhel"
+			),
+			array(
+				"district" => "lalitpur",
+				"headquarter" => "patan"
+			),
+			array(
+				"district" => "nuwakot",
+				"headquarter" => "bidur"
+			),
+			array(
+				"district" => "rasuwa",
+				"headquarter" => "dhunche"
+			),
+			array(
+				"district" => "sindhupalchok",
+				"headquarter" => "chautara"
+			),
+			
+		),
+	"bheri" => array(
+			array(
+				"district" => "banke",
+				"headquarter" => "nepalgunj"
+			),
+			array(
+				"district" => "bardiya",
+				"headquarter" => "gulariya"
+			),
+			array(
+				"district" => "dailekh",
+				"headquarter" => "dullu"
+			),
+			array(
+				"district" => "jajarkot",
+				"headquarter" => "khalanga"
+			),
+			array(
+				"district" => "surkhet",
+				"headquarter" => "surkhet"
+			),
+			
+		),
+	"dhawalagiri" => array(
+			array(
+				"district" => "baglung",
+				"headquarter" => "baglung"
+			),
+			array(
+				"district" => "mustang",
+				"headquarter" => "jomsom"
+			),
+			array(
+				"district" => "myagdi",
+				"headquarter" => "beni"
+			),
+			array(
+				"district" => "parbat",
+				"headquarter" => "kusma"
+			),
+			
+		),
+	"gandaki" => array(
+			array(
+				"district" => "gorkha",
+				"headquarter" => "gorkha"
+			),
+			array(
+				"district" => "kaski",
+				"headquarter" => "pokhara"
+			),
+			array(
+				"district" => "lamjung",
+				"headquarter" => "bensi sahar"
+			),
+			array(
+				"district" => "manang",
+				"headquarter" => "chame"
+			),
+			array(
+				"district" => "syangja",
+				"headquarter" => "syangja"
+			),
+			array(
+				"district" => "tanahu",
+				"headquarter" => "damauli"
+			),
+			
+		),
+	"janakpur" => array(
+			array(
+				"district" => "dhanusa",
+				"headquarter" => "janakpur"
+			),
+			array(
+				"district" => "dholkha",
+				"headquarter" => "charikot"
+			),
+			array(
+				"district" => "mahottari",
+				"headquarter" => "jaleswor"
+			),
+			array(
+				"district" => "ramechhap",
+				"headquarter" => "manthali"
+			),
+			array(
+				"district" => "sarlahi",
+				"headquarter" => "malangwa"
+			),
+			array(
+				"district" => "sindhuli",
+				"headquarter" => "sindhuli madhi"
+			),
+			
+		),
+	"karnali" => array(
+			array(
+				"district" => "dolpa",
+				"headquarter" => "dolpa"
+			),
+			array(
+				"district" => "humla",
+				"headquarter" => "simikot"
+			),
+			array(
+				"district" => "jumla",
+				"headquarter" => "jumla khalanga"
+			),
+			array(
+				"district" => "kalikot",
+				"headquarter" => "kalikot"
+			),
+			array(
+				"district" => "mugu",
+				"headquarter" => "gamgadhi"
+			),
+			
+		),
+	"koshi" => array(
+			array(
+				"district" => "bhojpur",
+				"headquarter" => "bhojpur"
+			),
+			array(
+				"district" => "dhankuta",
+				"headquarter" => "dhankuta"
+			),
+			array(
+				"district" => "morang",
+				"headquarter" => "biratnagar"
+			),
+			array(
+				"district" => "sankhuwasabha",
+				"headquarter" => "khandbari"
+			),
+			array(
+				"district" => "sunsari",
+				"headquarter" => "inaruwa"
+			),
+			array(
+				"district" => "terhathum",
+				"headquarter" => "manglung"
+			),
+			
+		),
+	"lumbini" => array(
+			array(
+				"district" => "arghakhanchi",
+				"headquarter" => "sandhikharka"
+			),
+			array(
+				"district" => "gulmi",
+				"headquarter" => "tamghas"
+			),
+			array(
+				"district" => "kapilvastu",
+				"headquarter" => "taulihawa"
+			),
+			array(
+				"district" => "nawalparasi",
+				"headquarter" => "parasi"
+			),
+			array(
+				"district" => "palpa",
+				"headquarter" => "tansen"
+			),
+			array(
+				"district" => "rupandehi",
+				"headquarter" => "bhairahawa"
+			),
+			
+		),
+	"mahakali" => array(
+			array(
+				"district" => "baitadi",
+				"headquarter" => "baitadi"
+			),
+			array(
+				"district" => "dadeldhura",
+				"headquarter" => "dadeldhura"
+			),
+			array(
+				"district" => "darchula",
+				"headquarter" => "darchula"
+			),
+			array(
+				"district" => "kanchanpur",
+				"headquarter" => "mahendara nagar"
+			),
+			
+		),
+	"mechi" => array(
+			array(
+				"district" => "ilam",
+				"headquarter" => "ilam"
+			),
+			array(
+				"district" => "jhapa",
+				"headquarter" => "chandragadhi"
+			),
+			array(
+				"district" => "panchthar",
+				"headquarter" => "phidim"
+			),
+			array(
+				"district" => "taplejung",
+				"headquarter" => "taplejung"
+			),
+			
+		),
+	"narayani" => array(
+			array(
+				"district" => "bara",
+				"headquarter" => "kalaiya"
+			),
+			array(
+				"district" => "chitwan",
+				"headquarter" => "bharatpur"
+			),
+			array(
+				"district" => "makwanpur",
+				"headquarter" => "hetauda"
+			),
+			array(
+				"district" => "parsa",
+				"headquarter" => "birgunj"
+			),
+			array(
+				"district" => "rautahat",
+				"headquarter" => "gaur"
+			),
+			
+		),
+	"rapti" => array(
+			array(
+				"district" => "dang deukhuri",
+				"headquarter" => "ghorahi"
+			),
+			array(
+				"district" => "pyuthan",
+				"headquarter" => "pyuthan khalanga"
+			),
+			array(
+				"district" => "rolpa",
+				"headquarter" => "liwang"
+			),
+			array(
+				"district" => "rukum",
+				"headquarter" => "musikot"
+			),
+			array(
+				"district" => "salyan",
+				"headquarter" => "salyan khalanga"
+			),
+			
+		),
+	"sagarmatha" => array(
+			array(
+				"district" => "khotang",
+				"headquarter" => "diktel"
+			),
+			array(
+				"district" => "okhaldhunga",
+				"headquarter" => "okhaldhunga"
+			),
+			array(
+				"district" => "saptari",
+				"headquarter" => "rajbiraj"
+			),
+			array(
+				"district" => "siraha",
+				"headquarter" => "siraha"
+			),
+			array(
+				"district" => "solukhumbu",
+				"headquarter" => "salleri"
+			),
+			array(
+				"district" => "udayapur",
+				"headquarter" => "gaighat"
+			),
+			
+		),
+	"seti" => array(
+			array(
+				"district" => "achham",
+				"headquarter" => "mangalsen"
+			),
+			array(
+				"district" => "bajhang",
+				"headquarter" => "chainpur"
+			),
+			array(
+				"district" => "bajura",
+				"headquarter" => "martadi"
+			),
+			array(
+				"district" => "doti",
+				"headquarter" => "dipayal"
+			),
+			array(
+				"district" => "kailali",
+				"headquarter" => "dhangadhi"
+			),
+			
+		)
+);
+?>
