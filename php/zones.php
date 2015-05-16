6046bd6aed541270e4e5b364d77e6933455eea27
Sandip Bhagat <sandipbgt@gmail.com>
2015-05-16 09:08:23 +0545

    Added list of zones in PHP array format

---
+++ php/zones.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Sandip Bhagat
+ * @website www.sandipbhagat.com
+ * @twitter sandipbgt
+ * @facebook sandip02
+ * @github https://github.com/sandipbgt/nepal-data
+ */
+ 
+$zones = array(
+			"bagmati",
+			"bheri",
+			"dhawalagiri",
+			"gandaki",
+			"janakpur",
+			"karnali",
+			"koshi",
+			"lumbini",
+			"mahakali",
+			"mechi",
+			"narayani",
+			"rapti",
+			"sagarmatha",
+			"seti"
+);
+?>
